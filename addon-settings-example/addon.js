5b7844a8a614c297c5b9398226bd6bc9b367de14
TheOwlCoder <100481826+TheOwlCoder@users.noreply.github.com>
2026-05-01 20:19:27 -0500

    Add addon.js

---
+++ addon-settings-example/addon.js
@@ -0,0 +1,10 @@
+addons.registerSettings("settings", (dialog) => {
+    dialog.content = `<p>Name:</p><textarea class="luduvoTextarea" role="nameInput">${localStorage.csnameSettings || ""}</textarea>`;
+    dialog.onConfirm = ()=>{
+        localStorage.setItem("csnameSettings", dialog.dialog.querySelector(`[role="nameInput"]`).value)
+    }
+})
+
+addons.navigationAddon("/dashboard", e => {
+    document.querySelector(".text-3xl.md\\:text-5xl.font-bold").innerHTML = (localStorage.csnameSettings)
+})
